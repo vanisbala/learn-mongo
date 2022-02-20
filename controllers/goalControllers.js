435977c62ca3dc1120f69446797a7013595b55c0
vanibala <vanisbala@gmail.com>
2022-02-19 17:53:53 -0800

    Added the complete CRUD for mongo using express

--- controllers/goalControllers.js
+++ controllers/goalControllers.js
@@ -1,11 +1,12 @@
-asyncHandler = require(express-async-handler)
-
+const asyncHandler = require('express-async-handler')
+const Goal = require('../models/goalmodel')
 
 // @desc get Goals
 // @route GET api/goals
 // @access private
 const getGoals =asyncHandler( async(req,res) => {
-    res.status(200).json({ message: 'get Goals'})
+    const goals = await Goal.find()
+    res.status(200).json(goals)
 })
 
 
@@ -17,9 +18,12 @@ const setGoals =asyncHandler( async (req,res) => {
         res.status(400)
         throw new Error('Please add a text field')
     }
+
+    const goal = await Goal.create({
+        text: req.body.text,
+    })
    
-      res.json({ message: 'Please Enter some text'})
- 
+      res.status(200).json(goal)
     
 })
 
@@ -27,16 +31,48 @@ const setGoals =asyncHandler( async (req,res) => {
 // @desc update Goals
 // @route PUT api/goals/:id
 // @access private
-const updateGoals =asyncHandler( async (req,res) => {
-    res.status(200).json({ message: `update Goals ${req.params.id}`})
-}
-)
+const updateGoals = asyncHandler(async (req, res) => {
+    const goal = await Goal.findById(req.params.id)
+  
+    if (!goal) {
+      res.status(400)
+      throw new Error('Goal not found')
+    }
+  
+    // // Check for user
+    // if (!req.user) {
+    //   res.status(401)
+    //   throw new Error('User not found')
+    // }
+  
+    // // Make sure the logged in user matches the goal user
+    // if (goal.user.toString() !== req.user.id) {
+    //   res.status(401)
+    //   throw new Error('User not authorized')
+    // }
+  
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    })
+  
+    res.status(200).json(updatedGoal)
+  })
 
 // @desc delete Goals
 // @route DELETE api/goals/:id
 // @access private
 const deleteGoals =asyncHandler( async (req,res) => {
-    res.status(200).json({ message: `delete Goals ${req.params.id}`})
+
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    await goal.remove()
+    res.status(200).json({ id: req.params.id })
+
 })
 
 module.exports = {
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const colors = require('colors')
 const {errorHandler} = require('./errorMiddleware/errorMiddleware')
 const port = process.env.PORT || 5000
 
-
+const connectDB = require('./config/db')
 const app = express()
+connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
